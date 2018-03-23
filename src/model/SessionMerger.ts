33158dedddb50e0100a0092e3935f4b4ac4e93c0
JulianG <juliangaramendy@gmail.com>
2018-03-23 08:03:04 +0000

    cleaned up merger and its logging

--- src/model/SessionMerger.ts
+++ src/model/SessionMerger.ts
@@ -1,121 +1,121 @@
 import * as BT from './CoreTypes';
 
 import MutedConsole from '../utils/MutedConsole';
-const console = new MutedConsole();
+const _console = new MutedConsole();
+// const _console = console; // enable logging
 
 export default class SessionMerger {
 
 	mergeSessions(live: BT.Session, stored: BT.Session): BT.Session {
 
-		console.group('SessionMerger.mergeSessions');
+		_console.group('SessionMerger.mergeSessions');
 
 		const mergedSessionWindows: BT.Window[] = [];
 
 		stored.windows.forEach((storedWindow, storedWindowIndex) => {
 
-			console.group('storedWindow: ' + storedWindowIndex);
-
-			console.log('processing stored window:', storedWindow.title, storedWindow.id);
+			_console.group('processing a stored window: ' + storedWindow.id + ' ' + storedWindow.title);
 
 			if (storedWindow.visible === false) {
-
+				_console.log('window is hidden...');
 				if (storedWindow.title !== '') {
-					console.log('  pushing a hidden window: ' + storedWindow.title);
+					_console.log('  pushing a hidden window: ' + storedWindow.title);
 					storedWindow.focused = false;
 					this.pushUniqueWindow(mergedSessionWindows, storedWindow);
+				} else {
+					_console.warn('NOT pushing hidden window because title was empty string.');
 				}
 			} else {
-				console.log('  window ' + storedWindow.id + ' ' + storedWindow.title + ' is visible...');
-				console.log('  looking for a live matching window...');
+				_console.log('window is visible...');
+				_console.log('looking for a live matching window...');
 
 				const liveMatchingWindow = live.windows.find(liveWindow => this.compareWindows(liveWindow, storedWindow) > 0.75);
 				if (liveMatchingWindow) {
-					console.log('  found liveMatchingWindow: ' + liveMatchingWindow.id + ' ' + liveMatchingWindow.title);
+					_console.log('found liveMatchingWindow: ' + liveMatchingWindow.id + ' ' + liveMatchingWindow.title);
 					liveMatchingWindow.title = storedWindow.title;
 					liveMatchingWindow.expanded = storedWindow.expanded;
 					liveMatchingWindow.tabs = this.mergeTabs(liveMatchingWindow.tabs, storedWindow.tabs);
-					console.log('  pushing live matching window: ' + liveMatchingWindow.title);
+					_console.log('pushing live matching window: ' + liveMatchingWindow.id + ' ' + liveMatchingWindow.title);
 					this.pushUniqueWindow(mergedSessionWindows, liveMatchingWindow);
 
 				} else {
-					console.log('  could not find a live matching window');
+					_console.log('could not find a live matching window');
 
 					if (storedWindow.title !== '') {
-						console.log('  pushing a hidden window: ' + storedWindow.title);
+						_console.log('pushing a hidden window: ' + storedWindow.id + ' ' + storedWindow.title);
 						storedWindow.focused = false;
 						storedWindow.visible = false;
 						this.pushUniqueWindow(mergedSessionWindows, storedWindow);
+					} else {
+						_console.log('NOT pushing stored window because title was empty string.');
 					}
 				}
 			}
-			console.groupEnd();
+			_console.groupEnd();
 		});
 
-		const nonMatchedWindows = live.windows.filter(liveWindow => {
-			return !mergedSessionWindows.some(sessionWindow => sessionWindow.id === liveWindow.id);
+		const chromeExtensionWindow = this.findChromeExtensionWindow(live.windows);
+		const nonMatchedWindows = live.windows.filter(liveW => {
+			return liveW !== chromeExtensionWindow && !mergedSessionWindows.some(msW => msW.id === liveW.id);
 		});
+
+		_console.group('adding nonMatchedWindows...');
 		const newSessionWindows = [...mergedSessionWindows, ...nonMatchedWindows];
+		_console.table(nonMatchedWindows);
+		_console.groupEnd();
 
-		const extensionWindow = this.findExtensionWindow(newSessionWindows);
-		const filteredWindows = newSessionWindows.filter(w => w !== extensionWindow);
-		const panelGeometry: BT.Geometry = extensionWindow ?
-			extensionWindow.geometry
+		const panelGeometry: BT.Geometry = chromeExtensionWindow ?
+			chromeExtensionWindow.geometry
 			: { top: 0, left: 0, width: 0, height: 0 };
 
-		console.log('panelGeometry', panelGeometry);
-		console.groupEnd();
+		_console.groupEnd();
 
-		return { windows: filteredWindows, panelGeometry };
+		return { windows: newSessionWindows, panelGeometry };
 	}
 
 	private mergeTabs(liveTabs: BT.Tab[], storedTabs: BT.Tab[]): BT.Tab[] {
 
-		console.clear();
-		console.group('MERGETABS!');
-		console.log('storedTabs...');
-		console.table(storedTabs);
-		console.log('liveTabs...');
-		console.table(liveTabs);
+		_console.groupCollapsed('mergeTabs:');
+		_console.log('storedTabs...');
+		_console.table(storedTabs);
+		_console.log('liveTabs...');
+		_console.table(liveTabs);
 
+		_console.log('extraLiveTabs... (tabs in liveTabs not present in storedTabs)');
 		const extraLiveTabs = liveTabs.filter(liveTab => {
 			return (storedTabs.find(storedTab => storedTab.id === liveTab.id) === undefined);
 		});
+		_console.table(extraLiveTabs);
 
-		console.log('extraLiveTabs... (tabs in liveTabs not present in storedTabs)');
-		console.table(extraLiveTabs);
-
+		_console.log(`filteredTabs... 
+		(storedTabs which are either not visible or are visible AND have a liveTab with the same id)`);
 		const filteredTabs: BT.Tab[] = storedTabs.filter((storedTab, i) => {
 			return storedTab.visible === false ||
 				storedTab.visible && liveTabs.find(liveTab => liveTab.id === storedTab.id);
 		});
+		_console.table(filteredTabs);
 
-		console.log(`filteredTabs... 
-		(storedTabs which are either not visible or are visible AND have a liveTab with the same id)`);
-		console.table(filteredTabs);
-
+		_console.log(`mergedTabs... 
+		(filteredTabs with extraLiveTabs inserted by index... hmmmm not accurate enough?)`);
 		const mergedTabs = [...filteredTabs];
 		extraLiveTabs.forEach(t => {
 			mergedTabs.splice(t.index, 0, t);
 		});
+		_console.table(mergedTabs);
 
-		console.log(`mergedTabs... 
-		(filteredTabs with extraLiveTabs inserted by index... hmmmm not accurate enough?)`);
-		console.table(mergedTabs);
-
+		_console.log('mergedLiveTabs... mergedTabs fixed to get details from liveTabs where possible');
 		let highestLiveTabIndex = -1;
 		const mergedLiveTabs = mergedTabs.map((tab, i) => {
 			const liveTab = liveTabs.find(lt => lt.id === tab.id);
 			highestLiveTabIndex = liveTab ? liveTab.index + 1 : highestLiveTabIndex;
-			const xTab = liveTab || tab;
-			xTab.title = (xTab.title !== '') ? xTab.title : tab.title;
-			xTab.icon = (xTab.icon !== '') ? xTab.icon : tab.icon;
-			xTab.index = (xTab === tab) ? highestLiveTabIndex : xTab.index;
-			xTab.listIndex = i;
-			return xTab;
+			const newTab = { ...(liveTab || tab) };
+			newTab.title = newTab.title || tab.title;
+			newTab.icon = newTab.icon || tab.icon;
+			newTab.index = liveTab ? newTab.index : highestLiveTabIndex;
+			newTab.listIndex = i;
+			return newTab;
 		});
-
-		console.log('mergedLiveTabs... mergedTabs fixed to get details from liveTabs where possible');
-		console.table(mergedLiveTabs);
+		_console.table(mergedLiveTabs);
 
 		const sortedTabs = mergedLiveTabs.sort((a, b) => {
 			const aa = a.index + '.' + a.listIndex;
@@ -123,9 +123,9 @@ export default class SessionMerger {
 			return (aa > bb) ? 1 : (aa < bb) ? -1 : 0;
 		});
 
-		console.log('sortedTabs...');
-		console.table(sortedTabs);
-		console.groupEnd();
+		_console.log('sortedTabs...');
+		_console.table(sortedTabs);
+		_console.groupEnd();
 		return sortedTabs;
 
 	}
@@ -142,11 +142,11 @@ export default class SessionMerger {
 		if (array.some(w => w.id === window.id) === false) {
 			array.push(window);
 		} else {
-			console.warn('duplicate window: ', window.id, window.title);
+			_console.warn('duplicate window: ', window.id, window.title);
 		}
 	}
 
-	private findExtensionWindow(windows: BT.Window[]): BT.Window | undefined {
+	private findChromeExtensionWindow(windows: BT.Window[]): BT.Window | undefined {
 		return windows.find(w => {
 			return (w.tabs.some(t => t.url === chrome.extension.getURL('index.html')));
 		});
