5f586903c275b8ea3ad135ca9428b2c8f24e0bda
JulianG <juliangaramendy@gmail.com>
2018-03-27 17:57:19 +0100

    cleanup

--- sync-manifest-version.js
+++ sync-manifest-version.js
@@ -4,20 +4,20 @@ const exec = require('child_process').exec;
 const package = require('./package.json');
 const manifest_file = './public/manifest.json';
 
+const execLog = (err, stdout, stderr) => {
+	if (err == null) {
+		console.log(stdout);
+	} else {
+		console.error(stderr);
+	}
+};
+
 const manifest_text = fs.readFileSync(manifest_file);
 const manifest = JSON.parse(manifest_text);
-
-
 manifest.version = package.version;
 
 fs.writeFileSync(manifest_file, JSON.stringify(manifest, null, 2));
 // if (err) return console.log(err);
 console.log(`Updated ${manifest_file} with version ${package.version}`);
 
-let child = exec(`git add ${manifest_file}`, (err, stdout, stderr) => {
-	if (err == null) {
-		console.log(stdout);
-	} else {
-		console.error(stderr);
-	}
-});
+exec(`git add ${manifest_file}`, execLog);
