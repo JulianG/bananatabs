02df415d7450269a16fb7705c8528b56cf48d2cc
JulianG <juliangaramendy@gmail.com>
2018-03-27 10:18:39 +0100

    trying to call git add after version bump

--- sync-manifest-version.js
+++ sync-manifest-version.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const exec = require('child_process');
+
 const package = require('./package.json');
 const manifest_file = './public/manifest.json';
 
@@ -8,7 +10,16 @@ const manifest = JSON.parse(manifest_text);
 
 manifest.version = package.version;
 
-fs.writeFile(manifest_file, JSON.stringify(manifest, null, 2), function (err) {
-	if (err) return console.log(err);
-	console.log(`Updated ${manifest_file} with version ${package.version}`);
-  });
+fs.writeFileSync(manifest_file, JSON.stringify(manifest, null, 2));
+// if (err) return console.log(err);
+console.log(`Updated ${manifest_file} with version ${package.version}`);
+
+// must call shell script here to git add ./public/manifest.json
+
+let child = exec(`git add ${manifest_file}`, (err, stdout, stderr) => {
+	if (err == null) {
+		console.log(stdout);
+	} else {
+		console.error(stderr);
+	}
+});
