88a3ed9e855a6a269ae5705e3c3d3fd2c323dded
JulianG <juliangaramendy@gmail.com>
2018-03-27 10:19:40 +0100

    fixed bug

--- sync-manifest-version.js
+++ sync-manifest-version.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const exec = require('child_process');
+const exec = require('child_process').exec;
 
 const package = require('./package.json');
 const manifest_file = './public/manifest.json';
