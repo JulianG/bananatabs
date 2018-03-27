5c974140444b44b43c0aebb5aed30ac038f326a1
JulianG <juliangaramendy@gmail.com>
2018-03-27 17:56:56 +0100

    trying to add post-version step

---
+++ post-version.js
@@ -0,0 +1,13 @@
+const exec = require('child_process').exec;
+
+const zip_path = './dist/bananatabs-v';
+const manifest_file = './public/manifest.json';
+const manifest_text = fs.readFileSync(manifest_file);
+const manifest = JSON.parse(manifest_text);
+
+exec('npm run-script build', (err, stdout, stderr) => {
+
+	const zip_file = zip_path + manifest.version + '.zip';
+	exec(`zip ${zip_file} ./build `, execLog);
+
+});
