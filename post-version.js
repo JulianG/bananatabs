eb9c762f572da04952e34861ee3828e68754541d
JulianG <juliangaramendy@gmail.com>
2018-03-27 17:59:19 +0100

    fixed issue

--- post-version.js
+++ post-version.js
@@ -6,6 +6,14 @@ const manifest_file = './public/manifest.json';
 const manifest_text = fs.readFileSync(manifest_file);
 const manifest = JSON.parse(manifest_text);
 
+const execLog = (err, stdout, stderr) => {
+	if (err == null) {
+		console.log(stdout);
+	} else {
+		console.error(stderr);
+	}
+};
+
 exec('npm run-script build', (err, stdout, stderr) => {
 
 	const zip_file = zip_path + manifest.version + '.zip';
