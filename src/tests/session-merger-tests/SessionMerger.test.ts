d5afb7e2e338217aa73dd7ee64d51819135ca792
JulianG <juliangaramendy@gmail.com>
2018-04-01 00:11:35 +0100

    moved test files around

--- src/tests/session-merger-tests/SessionMerger.test.ts
+++ src/tests/session-merger-tests/SessionMerger.test.ts
@@ -7,9 +7,9 @@ const merger = new SessionMerger();
 
 test('when no windows visible. merged session is same as stored session', () => {
 
-	const liveSession: BT.Session = require('./empty-live-session.json');
-	const storedSession: BT.Session = require('./empty-stored-session.json');
-	const expectedSession: BT.Session = require('./empty-expected-session.json');
+	const liveSession: BT.Session = require('./no-windows/empty-live-session.json');
+	const storedSession: BT.Session = require('./no-windows/empty-stored-session.json');
+	const expectedSession: BT.Session = require('./no-windows/empty-expected-session.json');
 
 	const mergedSession = merger.mergeSessions(liveSession, storedSession);
 	expect(TestUtils.compareSessions(mergedSession, expectedSession)).toBeTruthy();
@@ -18,9 +18,9 @@ test('when no windows visible. merged session is same as stored session', () => 
 
 test('when no windows visible. after opening a new window.', () => {
 
-	const liveSession: BT.Session = require('./onetab-live-session.json');
-	const storedSession: BT.Session = require('./empty-stored-session.json');
-	const expectedSession: BT.Session = require('./onetab-expected-session.json');
+	const liveSession: BT.Session = require('./new-tab/onetab-live-session.json');
+	const storedSession: BT.Session = require('./new-tab/onetab-stored-session.json');
+	const expectedSession: BT.Session = require('./new-tab/onetab-expected-session.json');
 
 	const mergedSession = merger.mergeSessions(liveSession, storedSession);
 	expect(TestUtils.compareSessions(mergedSession, expectedSession)).toBeTruthy();
@@ -29,9 +29,9 @@ test('when no windows visible. after opening a new window.', () => {
 
 test('when there is only one tab and it is empty. onTabsUpdated status:"complete".', () => {
 
-	const liveSession: BT.Session = require('./example-live-session.json');
-	const storedSession: BT.Session = require('./example-stored-session.json');
-	const expectedSession: BT.Session = require('./example-expected-session.json');
+	const liveSession: BT.Session = require('./on-tab-status-complete/example-live-session.json');
+	const storedSession: BT.Session = require('./on-tab-status-complete/example-stored-session.json');
+	const expectedSession: BT.Session = require('./on-tab-status-complete/example-expected-session.json');
 
 	const mergedSession = merger.mergeSessions(liveSession, storedSession);
 	expect(TestUtils.compareSessions(mergedSession, expectedSession)).toBeTruthy();
