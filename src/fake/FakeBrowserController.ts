249ba2440ddc2d2c40919fb2cf5135aac54492bb
JulianG <juliangaramendy@gmail.com>
2018-05-23 16:34:13 +0100

    using Promise.resolve

--- src/fake/FakeBrowserController.ts
+++ src/fake/FakeBrowserController.ts
@@ -1,32 +1,26 @@
 import * as BT from '../model/CoreTypes';
 import BrowserController from '../model/mutators/BrowserController';
 
-function getResolvingPromise() {
-	return new Promise((resolve, reject) => {
-		setTimeout(resolve, 125);
-	});
-}
-
 export default class FakeBrowserController implements BrowserController {
 	public closeWindow(id: number) {
 		console.log(`FakeBrowserController.closeWindow(${id}) ...`);
-		return getResolvingPromise();
+		return Promise.resolve();
 	}
 	public closeTab(id: number) {
 		console.log(`FakeBrowserController.closeTab(${id}) ...`);
-		return getResolvingPromise();
+		return Promise.resolve();
 	}
 	public selectTab(id: number) {
 		console.log(`FakeBrowserController.selectTab(${id}) ...`);
-		return getResolvingPromise();
+		return Promise.resolve();
 	}
 	public createTab(window: BT.Window, tab: BT.Tab) {
 		console.log(`FakeBrowserController.createTab(...) ...`);
-		return getResolvingPromise();
+		return Promise.resolve();
 	}
 
 	public showWindow(window: BT.Window, first: boolean) {
 		console.log(`FakeBrowserController.showWindow(...) ...`);
-		return getResolvingPromise();
+		return Promise.resolve();
 	}
 }
