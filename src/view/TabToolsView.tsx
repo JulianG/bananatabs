a2a4872885b4c497dfe01a834fa4d94a09c11aed
JulianG <juliangaramendy@gmail.com>
2018-06-12 22:35:32 +0100

    tootip

--- src/view/TabToolsView.tsx
+++ src/view/TabToolsView.tsx
@@ -19,40 +19,54 @@ interface Props {
 	onCopyAction?(): void;
 }
 
-export default class TabToolsView extends React.Component<Props, {}> {
+interface State {
+	tooltip: string;
+}
+
+export default class TabToolsView extends React.Component<Props, State> {
 
 	constructor(props: Props) {
 		super(props);
+		this.state = { tooltip: '' };
 		this.handleRenameAction = this.handleRenameAction.bind(this);
 		this.handleDeleteAction = this.handleDeleteAction.bind(this);
 		this.handleCopyAction = this.handleCopyAction.bind(this);
+		this.handleMouseOver = this.handleMouseOver.bind(this);
+		this.resetToolTip = this.resetToolTip.bind(this);
 	}
 
 	render() {
 
 		return (
-			<div className="tab-tools">
+			<div className="tab-tools" onMouseOut={this.resetToolTip}>
+				<span className="tooltip">{this.state.tooltip}</span>&nbsp;
 				{this.props.actionIconVisibility.copy &&
 					<img
+						id={'share'}
 						title="Copy"
 						className="icon"
 						src={Icons.Copy}
 						onClick={this.handleCopyAction}
+						onMouseOver={this.handleMouseOver}
 					/>
 				}
 				{this.props.actionIconVisibility.rename &&
 					<img
+						id={'rename'}
 						title="Rename"
 						className="icon"
 						src={Icons.Edit}
 						onClick={this.handleRenameAction}
+						onMouseOver={this.handleMouseOver}
 					/>}
 				{this.props.actionIconVisibility.delete &&
 					<img
+						id={'delete'}
 						title="Close and Delete"
 						className="icon"
 						src={Icons.Delete}
 						onClick={this.handleDeleteAction}
+						onMouseOver={this.handleMouseOver}
 					/>}
 			</div>
 		);
@@ -66,7 +80,15 @@ export default class TabToolsView extends React.Component<Props, {}> {
 		(this.props.onDeleteAction || doNothing)();
 	}
 
-	private handleCopyAction(e: React.MouseEvent<HTMLElement>) {
+	private handleCopyAction(e: React.MouseEvent<HTMLImageElement>) {
 		(this.props.onCopyAction || doNothing)();
+	}
+
+	private handleMouseOver(e: React.MouseEvent<HTMLImageElement>) {
+		this.setState({tooltip: e.currentTarget.id});
+	}
+
+	private resetToolTip() {
+		this.setState({tooltip: ''});
 	}
 }
