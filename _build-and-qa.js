7792522c592563e7a1d01d83dcef10599271cdf9
JulianG <juliangaramendy@gmail.com>
2018-06-18 07:42:44 +0100

    build scritps

---
+++ _build-and-qa.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const exec = require('child_process').exec;
+
+const execLog = (err, stdout, stderr) => {
+	if (err == null) {
+		console.log(stdout);
+	} else {
+		console.error(stderr);
+	}
+};
+
+console.log('building...');
+exec('npm run-script build', (err, stdout, stderr) => {
+
+	if (err == null) {
+		console.log(stdout);
+		const cmd = `cp -R ./build ../bananatabs-qa-build`;
+		console.log('\n' + cmd);
+		exec(cmd, execLog);
+	} else {
+		console.error(stderr);
+	}
+
+});
