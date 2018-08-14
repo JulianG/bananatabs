3bb7a1c759db12b405e4b98fe607eb632c452f10
JulianG <juliangaramendy@gmail.com>
2018-08-14 06:50:09 +0100

    unused class

--- src/model/mutators/BrowserEventDispatcher.ts
+++ src/model/mutators/BrowserEventDispatcher.ts
@@ -1,11 +1,10 @@
-
-export interface BrowserEventListener {
-	(event: string, reason?: string): void;
-}
-
 export default interface BrowserEventDispatcher {
 	addListener(listener: BrowserEventListener): void;
 	removeListener(listener: BrowserEventListener): void;
 	enable(): void;
 	disable(): void;
+}
+
+interface BrowserEventListener {
+	(event: string, reason?: string): void;
 }
