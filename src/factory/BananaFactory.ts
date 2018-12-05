0552cbbaea2da428daa845ca394ce80858ba1e8b
JulianG <juliangaramendy@gmail.com>
2018-12-05 20:20:28 +0000

    auto formatting only

--- src/factory/BananaFactory.ts
+++ src/factory/BananaFactory.ts
@@ -7,9 +7,13 @@ import ChromeBrowserController from '../chrome/ChromeBrowserController';
 
 import SessionProvider from '../model/SessionProvider';
 import DefaultSessionProvider from '../model/DefaultSessionProvider';
-import LiveSessionMerger, { DefaultLiveSessionMerger } from '../model/mergers/LiveSessionMerger';
+import LiveSessionMerger, {
+  DefaultLiveSessionMerger
+} from '../model/mergers/LiveSessionMerger';
 import SessionPersistence from '../model/SessionPersistence';
-import SessionMutator, { DefaultSessionMutator } from '../model/mutators/SessionMutator';
+import SessionMutator, {
+  DefaultSessionMutator
+} from '../model/mutators/SessionMutator';
 
 import LocalStorageSessionPersistence from '../chrome/LocalStorageSessionPersistence';
 import RAMSessionPersistence from '../utils/test-utils/RAMSessionPersistence';
@@ -17,59 +21,57 @@ import RAMSessionPersistence from '../utils/test-utils/RAMSessionPersistence';
 const FAKE_INITIAL_SESSION = require('../utils/dev-utils/initial-session.json');
 
 export default class BananaFactory {
+  private chromeAPI: PromisingChromeAPI;
+  private persistence: SessionPersistence;
+  private liveSessionMerger: LiveSessionMerger;
+  private sessionProvider: SessionProvider;
+  private sessionMutator: SessionMutator;
+  private browserController: BrowserController;
 
-	private chromeAPI: PromisingChromeAPI;
-	private persistence: SessionPersistence;
-	private liveSessionMerger: LiveSessionMerger;
-	private sessionProvider: SessionProvider;
-	private sessionMutator: SessionMutator;
-	private browserController: BrowserController;
+  constructor(private isChromeAPIAvailable: boolean) {
+    this.chromeAPI = this.getChromeAPI();
+    this.persistence = this.getPersistence();
+    this.liveSessionMerger = new DefaultLiveSessionMerger();
+  }
 
-	constructor(private isChromeAPIAvailable: boolean) {
-		this.chromeAPI = this.getChromeAPI();
-		this.persistence = this.getPersistence();
-		this.liveSessionMerger = new DefaultLiveSessionMerger();
-	}
+  getBrowserController(): BrowserController {
+    if (!this.browserController) {
+      this.browserController = new ChromeBrowserController(this.chromeAPI);
+    }
+    return this.browserController;
+  }
 
-	getBrowserController(): BrowserController {
-		if (!this.browserController) {
-			this.browserController = new ChromeBrowserController(this.chromeAPI);
-		}
-		return this.browserController;
-	}
+  getSessionProvider(): SessionProvider {
+    if (!this.sessionProvider) {
+      this.sessionProvider = new DefaultSessionProvider(
+        this.getBrowserController(),
+        this.liveSessionMerger,
+        this.persistence
+      );
+    }
+    return this.sessionProvider;
+  }
 
-	getSessionProvider(): SessionProvider {
-		if (!this.sessionProvider) {
-			this.sessionProvider = new DefaultSessionProvider(
-				this.getBrowserController(),
-				this.liveSessionMerger,
-				this.persistence
-			);
-		}
-		return this.sessionProvider;
-	}
+  getSessionMutator(): SessionMutator {
+    if (!this.sessionMutator) {
+      this.sessionMutator = new DefaultSessionMutator(
+        this.getSessionProvider()
+      );
+    }
+    return this.sessionMutator;
+  }
 
-	getSessionMutator(): SessionMutator {
-		if (!this.sessionMutator) {
-			this.sessionMutator = new DefaultSessionMutator(
-				this.getSessionProvider()
-			);
-		}
-		return this.sessionMutator;
-	}
+  private getPersistence() {
+    if (this.isChromeAPIAvailable) {
+      return new LocalStorageSessionPersistence();
+    } else {
+      return new RAMSessionPersistence(FAKE_INITIAL_SESSION.storedSession);
+    }
+  }
 
-	private getPersistence() {
-		if (this.isChromeAPIAvailable) {
-			return new LocalStorageSessionPersistence();
-		} else {
-			return new RAMSessionPersistence(FAKE_INITIAL_SESSION.storedSession);
-		}
-	}
-
-	private getChromeAPI() {
-		return (this.isChromeAPIAvailable) ?
-			new RealPromisingChromeAPI() :
-			initialiseFakeChromeAPI(FAKE_INITIAL_SESSION.liveSession); // this could be a BT.Session as well
-	}
-
+  private getChromeAPI() {
+    return this.isChromeAPIAvailable
+      ? new RealPromisingChromeAPI()
+      : initialiseFakeChromeAPI(FAKE_INITIAL_SESSION.liveSession); // this could be a BT.Session as well
+  }
 }
