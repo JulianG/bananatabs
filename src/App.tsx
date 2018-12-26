c2e136396df426cc7b06b55f42e6d0b716fbb0dc
JulianG <juliangaramendy@gmail.com>
2018-12-26 21:19:08 +0000

    renamed class

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import BananaTabs from './BananaTabs';
 import BananaFactory from './factory/BananaFactory';
 import * as FakeInitialState from './utils/dev-utils/fake-initial-state';
 import { stringToSession } from './serialisation/MarkdownSerialisation';
-import FakeChrome from './chrome-api/FakeChrome';
-import FakePromisingChromeAPI from './chrome-api/FakePromisingChromeAPI';
+import ChromeAPIView from './chrome-api/ChromeAPIView';
 
 const hasChrome = !!(chrome && chrome.windows);
 
@@ -20,17 +19,15 @@ if (hasChrome) {
   App = () => <BananaTabs factory={factory} />;
 } else {
   App = () => {
-    const fchrome = factory.getChromeAPI() as FakePromisingChromeAPI;
+    const chromeAPI = factory.getChromeAPI();
 
-    // tslint:disable no-string-literal
-    window['fchrome'] = fchrome;
     return (
       <div className="split">
         <div className="split-bananatabs">
           <BananaTabs factory={factory} />
         </div>
         <div className="split-browser">
-          <FakeChrome api={fchrome} />
+          <ChromeAPIView chromeAPI={chromeAPI} />
         </div>
       </div>
     );
---
+++ src/chrome-api/ChromeAPIView.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import { PromisingChromeAPI } from './PromisingChromeAPI';
+import BrowserEventDispatcher from '../model/mutators/BrowserEventDispatcher';
+import ChromeEventDispatcher from '../chrome/ChromeEventDispatcher';
+
+type Props = {
+  chromeAPI: PromisingChromeAPI;
+};
+
+type State = {
+  windows: chrome.windows.Window[];
+};
+
+export default class ChromeAPIView extends React.Component<Props, State> {
+  private browserEventDispatcher: BrowserEventDispatcher;
+
+  constructor(props: Props) {
+    super(props);
+    this.state = { windows: [] };
+    this.browserEventDispatcher = new ChromeEventDispatcher(props.chromeAPI);
+  }
+
+  componentDidMount() {
+    this.browserEventDispatcher.addListener(this.browserEventHandler);
+  }
+
+  componentWillUnmount() {
+    this.browserEventDispatcher.removeListener(this.browserEventHandler);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Fake Chrome</h2>
+        <pre>{stateToString(this.state.windows)}</pre>
+      </div>
+    );
+  }
+
+  browserEventHandler = async () => {
+    const windows = await this.props.chromeAPI.windows.getAll({});
+    this.setState({ windows });
+  }
+}
+
+function stateToString(windows: chrome.windows.Window[]): string {
+  return windows.map(windowToString).join('\n');
+}
+
+function windowToString(w: chrome.windows.Window): string {
+  return `${w.id} (window):\n` + (w.tabs || []).map(tabToString).join('\n') + '\n';
+}
+
+function tabToString(t: chrome.tabs.Tab): string {
+  const title = t.title !== t.url && t.title !== '' && t.title !== undefined ? t.title + ' ' : '';
+  return `- ${t.id} - ${title}${t.url || ''} ${t.status || ''}`;
+}
