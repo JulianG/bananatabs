58bb140e12434e15a99c2a9983aa638a6aaea99f
JulianG <juliangaramendy@gmail.com>
2018-12-28 19:00:39 +0000

    mostly formatting - also adding 1 function

--- src/_test-utils/fake-chrome-test-utils.ts
+++ src/_test-utils/fake-chrome-test-utils.ts
@@ -3,33 +3,39 @@ import { AllCallbacks } from './expect-extend-functions';
 
 export function getAllCallbacks(fchrome: FakePromisingChromeAPI) {
 
-	const allCallbacks: AllCallbacks = [
-		{ id: 'chrome.windows.onCreated', event: fchrome.windows.onCreated, callback: jest.fn() },
-		{ id: 'chrome.windows.onFocusChanged', event: fchrome.windows.onFocusChanged, callback: jest.fn() },
-		{ id: 'chrome.windows.onRemoved', event: fchrome.windows.onRemoved, callback: jest.fn() },
-		{ id: 'chrome.tabs.onCreated', event: fchrome.tabs.onCreated, callback: jest.fn() },
-		{ id: 'chrome.tabs.onActivated', event: fchrome.tabs.onActivated, callback: jest.fn() },
-		{ id: 'chrome.tabs.onAttached', event: fchrome.tabs.onAttached, callback: jest.fn() },
-		{ id: 'chrome.tabs.onMoved', event: fchrome.tabs.onMoved, callback: jest.fn() },
-		{ id: 'chrome.tabs.onRemoved', event: fchrome.tabs.onRemoved, callback: jest.fn() },
-		{ id: 'chrome.tabs.onUpdated', event: fchrome.tabs.onUpdated, callback: jest.fn() },
-		{ id: 'chrome.tabs.onHighlighted', event: fchrome.tabs.onHighlighted, callback: jest.fn() }
-	];
+  const allCallbacks: AllCallbacks = [
+    { id: 'chrome.windows.onCreated', event: fchrome.windows.onCreated, callback: jest.fn() },
+    { id: 'chrome.windows.onFocusChanged', event: fchrome.windows.onFocusChanged, callback: jest.fn() },
+    { id: 'chrome.windows.onRemoved', event: fchrome.windows.onRemoved, callback: jest.fn() },
+    { id: 'chrome.tabs.onCreated', event: fchrome.tabs.onCreated, callback: jest.fn() },
+    { id: 'chrome.tabs.onActivated', event: fchrome.tabs.onActivated, callback: jest.fn() },
+    { id: 'chrome.tabs.onAttached', event: fchrome.tabs.onAttached, callback: jest.fn() },
+    { id: 'chrome.tabs.onMoved', event: fchrome.tabs.onMoved, callback: jest.fn() },
+    { id: 'chrome.tabs.onRemoved', event: fchrome.tabs.onRemoved, callback: jest.fn() },
+    { id: 'chrome.tabs.onUpdated', event: fchrome.tabs.onUpdated, callback: jest.fn() },
+    { id: 'chrome.tabs.onHighlighted', event: fchrome.tabs.onHighlighted, callback: jest.fn() }
+  ];
 
-	hookAllCallbacks(allCallbacks);
+  hookAllCallbacks(allCallbacks);
 
-	return allCallbacks;
+  return allCallbacks;
 }
 
 function hookAllCallbacks(allCallbacks: AllCallbacks) {
-	allCallbacks.forEach(entry => {
-		entry.callback.mockReset();
-		entry.event.addListener(entry.callback);
-	});
+  allCallbacks.forEach(entry => {
+    entry.callback.mockClear();
+    entry.callback.mockReset();
+    entry.event.addListener(entry.callback);
+  });
 }
 
 export function resetAllCallbacks(allCallbacks: AllCallbacks) {
-	allCallbacks.forEach(entry => {
-		entry.callback.mockReset();
-	});
+  allCallbacks.forEach(entry => {
+    entry.callback.mockClear();
+    entry.callback.mockReset();
+  });
+}
+
+export function countCalls(allCallbacks: AllCallbacks) {
+  console.log(allCallbacks.map(entry => `${entry.id}: ${entry.callback.mock.calls.length}`));
 }
