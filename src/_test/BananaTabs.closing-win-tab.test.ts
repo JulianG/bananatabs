20a86984d3bba3324bff751cf7af84506ea02b2d
JulianG <juliangaramendy@gmail.com>
2018-12-30 17:21:39 +0000

    more integration tests

--- src/_test/BananaTabs.closing-win-tab.test.ts
+++ src/_test/BananaTabs.closing-win-tab.test.ts
@@ -1,29 +1,18 @@
 import 'react-testing-library/cleanup-after-each';
 
-// import { fireEvent /*, getByTestId, cleanup*/ } from 'react-testing-library';
-
-import {
-  getWindowGroups,
-  getTabsVisibilities,
-  getTabsInWindow,
-  renderBananaTabs
-} from '../_test-utils/bananatabs.utils';
-
-import { wait, compareSessions } from '../_test-utils';
-
-// import { stringToSession } from '../serialisation/MarkdownSerialisation';
+import { renderBananaTabs } from '../_test-utils/bananatabs.utils';
+import { wait } from '../_test-utils';
 
 describe('BananaTabs Tests: Interaction from browser', async () => {
   //
   test('closing one of many tabs (from the fake browser)', async () => {
     // given an initial state with 1 window and 3 tabs
-    const { container, fchrome, provider } = await renderBananaTabs(`
+    const { fchrome, provider } = await renderBananaTabs(`
       window 1:
       * http://tab-1.1/
       * http://tab-1.2/
       * http://tab-1.3/
     `);
-    await wait(1);
 
     const tabId = fchrome.fakeWindows[0].tabs![2].id!;
 
@@ -37,7 +26,7 @@ describe('BananaTabs Tests: Interaction from browser', async () => {
 
   test('closing the only tab in a window (from the fake browser)', async () => {
     // given an initial state with 2 windows
-    const { container, fchrome, provider } = await renderBananaTabs(`
+    const { fchrome, provider } = await renderBananaTabs(`
       window 1:
       * http://tab-1.1/
       * http://tab-1.2/
@@ -46,13 +35,12 @@ describe('BananaTabs Tests: Interaction from browser', async () => {
       window 2:
       * http://tab-2.1/
     `);
-    await wait(1);
 
     const tabId = fchrome.fakeWindows[1].tabs![0].id!;
 
     // when closing the only tab in a window
     fchrome.tabs.remove(tabId);
-    await wait(100);
+    await wait();
 
     // expect only 1 window in the fake browser
     const fchws = await fchrome.windows.getAll({});
@@ -64,14 +52,60 @@ describe('BananaTabs Tests: Interaction from browser', async () => {
     // the affected window is now hidden and it still contains the last tab
     expect(provider.session.windows[1].visible).toBe(false);
     expect(provider.session.windows[1].tabs).toHaveLength(1);
-
   });
 
-  test.skip('closing unnamed window from the browser', () => {
-    // TODO: write this
+  test('closing unnamed window from the browser', async () => {
+    // given an initial state with 2 windows
+    const { fchrome, provider } = await renderBananaTabs(`
+      window 1:
+      * http://tab-1.1/
+      * http://tab-1.2/
+      * http://tab-1.3/
+      * 
+      :
+      * http://tab-2.1/
+      * http://tab-2.2/
+      * http://tab-2.3/
+    `);
+
+    // when closing an unnamed window
+    await fchrome.windows.remove(fchrome.fakeWindows[1].id!);
+    await wait();
+
+    // expect only 1 window in the fake browser
+    const fchws = await fchrome.windows.getAll({});
+    expect(fchws).toHaveLength(1);
+
+    // expect only 1 window in bananatabs
+    expect(provider.session.windows).toHaveLength(1);
   });
 
-  test.skip('closing named window from the browser', () => {
-    // TODO: write this
+  test('closing named window from the browser', async () => {
+    // given an initial state with 2 windows
+    const { fchrome, provider } = await renderBananaTabs(`
+      window 1:
+      * http://tab-1.1/
+      * http://tab-1.2/
+      * http://tab-1.3/
+      * 
+      :
+      * http://tab-2.1/
+      * http://tab-2.2/
+      * http://tab-2.3/
+    `);
+
+    // when closing an unnamed window
+    await fchrome.windows.remove(fchrome.fakeWindows[0].id!);
+    await wait();
+
+    // expect only 1 window in the fake browser
+    const fchws = await fchrome.windows.getAll({});
+    expect(fchws).toHaveLength(1);
+
+    // expect 2 windows in bananatabs
+    expect(provider.session.windows).toHaveLength(2);
+
+    // expect the affected window to be hidden
+    expect(provider.session.windows[0].visible).toBe(false);
   });
 });
