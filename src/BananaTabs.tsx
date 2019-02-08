0fa40afe03e6774dfb46b8e83580c4c5b71c8637
JulianG <juliangaramendy@gmail.com>
2019-02-08 07:13:18 +0000

    stateless BananaTabs.tsx

--- src/BananaTabs.tsx
+++ src/BananaTabs.tsx
@@ -1,63 +1,53 @@
 import * as React from 'react';
-import * as BT from './model/core/CoreTypes';
 import { BananaContext } from './context/BananaContext';
 import { MainView } from './view/MainView';
 
 const MANIFEST = require('./manifest.lnk.json');
 
+console.assert(
+  MANIFEST.version !== undefined,
+  'manifest.json must contain a "version" key.'
+);
+
 interface Props {
   context: BananaContext;
 }
 
-interface State {
-  session: BT.Session;
-}
-
-export class BananaTabs extends React.Component<Props, State> {
-  readonly state: State = { session: BT.EmptySession };
+export class BananaTabs extends React.Component<Props> {
   private version: string;
   private buildString: string;
 
   constructor(props: Props) {
     super(props);
-    console.assert(
-      MANIFEST.version !== undefined,
-      'manifest.json must contain a "version" key.'
-    );
     this.version = MANIFEST.version || '0.0';
     this.buildString = '';
   }
 
-  shouldComponentUpdate(nextProps: Props, nextState: State): boolean {
-    return this.state.session !== nextState.session;
-  }
-
   componentDidMount() {
     window.addEventListener('resize', this.handleResizeEvent.bind(this));
-    this.props.context.sessionProvider.onSessionChanged = session => {
-      this.setState({ session });
+    this.props.context.sessionProvider.onSessionChanged = _ => {
+      this.forceUpdate();
     };
     this.props.context.sessionProvider.initialiseSession();
   }
 
   componentWillUnmount() {
     window.removeEventListener('resize', this.handleResizeEvent);
-    this.props.context.sessionProvider.onSessionChanged = session => {
-      // console.warn(
-      //   'BananaTabs.componentWillUnmount -> sessionProvider.onSessionChanged'
-      // );
+    this.props.context.sessionProvider.onSessionChanged = _ => {
+      /**/
     };
   }
 
   render() {
+    const { context } = this.props;
     return (
       <MainView
         version={this.version}
         buildString={this.buildString}
-        session={this.state.session}
-        sessionMutator={this.props.context.sessionMutator}
-        windowMutator={this.props.context.windowMutator}
-        tabMutator={this.props.context.tabMutator}
+        session={context.sessionProvider.session}
+        sessionMutator={context.sessionMutator}
+        windowMutator={context.windowMutator}
+        tabMutator={context.tabMutator}
       />
     );
   }
