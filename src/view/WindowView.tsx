fbeb0a5724d2c6f3e00b839869dd0e8d3420726f
JulianG <juliangaramendy@gmail.com>
2019-02-10 11:45:50 +0000

    WindowView component using hooks - first pass

--- src/view/WindowView.tsx
+++ src/view/WindowView.tsx
@@ -15,7 +15,7 @@ const Icons = {
   Off: require('./icons/off.svg'),
   Delete: require('./icons/delete.svg'),
   ArrowDown: require('./icons/arrow-down.svg'),
-  ArrowRight: require('./icons/arrow-right.svg')
+  ArrowRight: require('./icons/arrow-right.svg'),
 };
 
 interface Props {
@@ -25,216 +25,175 @@ interface Props {
   onCopy(windowId: number): void;
 }
 
-interface State {
-  toolsVisible: boolean;
-  renaming: boolean;
+export const WindowView = React.memo((props: Props) => {
+  const [areToolsVisible, setToolsVisible] = React.useState(false); // could be moved to renderHeader
+  const [renaming, setRenaming] = React.useState(false);
+
+  const w = props.window;
+
+  const styles = [
+    'window-group',
+    w.focused ? 'focused' : '',
+    areToolsVisible ? 'highlight' : '',
+    w.visible ? 'visible' : 'hidden',
+  ];
+
+  return (
+    <div
+      id="window-group"
+      data-testid="window-group"
+      className={styles.join(' ')}
+    >
+      {renderHeader(
+        props,
+        areToolsVisible,
+        setToolsVisible,
+        renaming,
+        setRenaming
+      )}
+      {CONFIG.debug && createDebugInfo(w, ['id'])}
+      {renderTabs(w, props.tabMutator)}
+    </div>
+  );
+}, shouldNotRerender);
+
+function shouldNotRerender(prevProps: Props, nextProps: Props) {
+  return compareWindow(prevProps.window, nextProps.window);
 }
 
-export class WindowView extends React.Component<Props, State> {
-  readonly state: State = { toolsVisible: false, renaming: false };
+function renderHeader(
+  props: Props,
+  areToolsVisible: boolean,
+  setToolsVisible: (b: boolean) => void,
+  renaming: boolean,
+  setRenaming: (b: boolean) => void
+) {
+  const w = props.window;
+  return (
+    <div
+      className="item-row"
+      onMouseEnter={() => setToolsVisible(true)}
+      onMouseLeave={() => setToolsVisible(false)}
+    >
+      {renderDisclosureButton(props)}
+      {renderVisibilityIcon(props)}
+      {!renaming && areToolsVisible && renderTools(props, setRenaming)}
+      {renaming
+        ? renderInputTitle(w, props.windowMutator, setRenaming)
+        : renderStaticTitle(w, setRenaming)}
+      {renaming && <div className="hint">&nbsp;Enter to save&nbsp;</div>}
+    </div>
+  );
+}
 
-  constructor(props: Props) {
-    super(props);
+function renderVisibilityIcon(props: Props) {
+  const w = props.window;
+  const visibilityIconSrc = w.visible ? Icons.On : Icons.Off;
+  const visibilityIconText = w.visible ? 'Hide Window' : 'Show Window';
+  const imgId = 'win-visibility' + (w.visible ? '-visible' : '-hidden');
 
-    this.handleStartRename = this.handleStartRename.bind(this);
-    this.handleCancelRename = this.handleCancelRename.bind(this);
-    this.handleSubmitRename = this.handleSubmitRename.bind(this);
-    this.handleDelete = this.handleDelete.bind(this);
-    this.handleCopy = this.handleCopy.bind(this);
-    this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
-    this.handleToggleCollapse = this.handleToggleCollapse.bind(this);
-    this.showTools = this.showTools.bind(this);
-    this.hideTools = this.hideTools.bind(this);
+  const toggleVisibility = () => {
+    w.visible
+      ? props.windowMutator.hideWindow(w.id)
+      : props.windowMutator.showWindow(w.id);
+  };
+
+  return (
+    <img
+      data-testid="visibility-toggle"
+      id={imgId}
+      alt={'win' + (w.visible ? '-visible' : '-hidden')}
+      className="tool icon"
+      src={visibilityIconSrc}
+      title={visibilityIconText}
+      onClick={toggleVisibility}
+    />
+  );
+}
+
+function renderTabs(window: BT.Window, tabMutator: TabMutator) {
+  const display = window.expanded ? 'block' : 'none';
+  return (
+    // window.expanded &&
+    <div style={{ display }}>
+      {window.tabs.map((tab, i) => {
+        const key = `tab-${tab.id}`; // `win-${window.id}/tab-${tab.id}`;
+        return (
+          <TabView key={key} window={window} tab={tab} mutator={tabMutator} />
+        );
+      })}
+    </div>
+  );
+}
+
+function renderDisclosureButton(props: Props) {
+  const w = props.window;
+  const iconSrc = w.expanded ? Icons.ArrowDown : Icons.ArrowRight;
+  const iconText = w.expanded ? 'Collapse' : 'Expand';
+  const iconStyles = ['tool', 'icon', w.visible ? '' : 'hidden'];
+
+  function toggleCollapse() {
+    w.expanded
+      ? props.windowMutator.collapseWindow(w.id)
+      : props.windowMutator.expandWindow(w.id);
   }
 
-  shouldComponentUpdate(nextProps: Props, nextState: State): boolean {
-    return (
-      compareWindow(nextProps.window, this.props.window) === false ||
-      nextState.toolsVisible !== this.state.toolsVisible ||
-      nextState.renaming !== this.state.renaming
-    );
-  }
+  return (
+    <img
+      id="disclosure"
+      alt={`window-disclosure-${w.expanded ? 'expanded' : 'collapsed'}`}
+      className={iconStyles.join(' ')}
+      src={iconSrc}
+      title={iconText}
+      onClick={toggleCollapse}
+    />
+  );
+}
 
-  render() {
+function renderInputTitle(
+  window: BT.Window,
+  windowMutator: WindowMutator,
+  setRenaming: (b: boolean) => void
+): JSX.Element {
+  const title = window.title;
 
-    console.log('WindowView.render');
-    
-    const w = this.props.window;
+  const submitRename = (text: string) => {
+    windowMutator.renameWindow(window.id, text);
+    setRenaming(false);
+  };
 
-    const styles = [
-      'window-group',
-      w.focused ? 'focused' : '',
-      this.state.toolsVisible ? 'highlight' : '',
-      w.visible ? 'visible' : 'hidden'
-    ];
+  return (
+    <InputForm
+      className="window-title"
+      text={title}
+      onSubmit={submitRename}
+      onCancel={() => setRenaming(false)}
+    />
+  );
+}
 
-    return (
-      <div
-        id="window-group"
-        data-testid="window-group"
-        className={styles.join(' ')}
-      >
-        {this.renderHeader()}
-        {CONFIG.debug && createDebugInfo(w, ['id'])}
-        {this.renderTabs()}
-      </div>
-    );
-  }
+function renderStaticTitle(
+  window: BT.Window,
+  setRenaming: (b: boolean) => void
+): JSX.Element {
+  const tabsStr = window.expanded ? '' : ' (' + window.tabs.length + ' tabs)';
+  const title = window.title || 'Window';
+  const fullscreen = window.state === 'fullscreen' ? '(fullscreen)' : '';
 
-  private renderHeader() {
-    const w = this.props.window;
-    return (
-      <div
-        className="item-row"
-        onMouseEnter={this.showTools}
-        onMouseLeave={this.hideTools}
-      >
-        {this.renderDisclosureButton()}
-        {this.renderVisibilityIcon()}
-        {!this.state.renaming && this.renderTools()}
-        {this.state.renaming
-          ? this.renderInputTitle(w)
-          : this.renderStaticTitle(w)}
-        {this.state.renaming && (
-          <div className="hint">&nbsp;Enter to save&nbsp;</div>
-        )}
-      </div>
-    );
-  }
+  return (
+    <span className="window-title" onClick={() => setRenaming(true)}>
+      {title} <span>{tabsStr}</span> <span>{fullscreen}</span>
+    </span>
+  );
+}
 
-  private renderVisibilityIcon() {
-    const w = this.props.window;
-    const visibilityIconSrc = w.visible ? Icons.On : Icons.Off;
-    const visibilityIconText = w.visible ? 'Hide Window' : 'Show Window';
-    const imgId = 'win-visibility' + (w.visible ? '-visible' : '-hidden');
-
-    return (
-      <img
-        data-testid="visibility-toggle"
-        id={imgId}
-        alt={'win' + (w.visible ? '-visible' : '-hidden')}
-        className="tool icon"
-        src={visibilityIconSrc}
-        title={visibilityIconText}
-        onClick={this.handleToggleVisibility}
-      />
-    );
-  }
-
-  private renderTabs() {
-    const { window, tabMutator } = this.props;
-    const display = window.expanded ? 'block' : 'none';
-    return (
-      // window.expanded &&
-      <div style={{ display }}>
-        {window.tabs.map((tab, i) => {
-          const key = `tab-${tab.id}`; // `win-${window.id}/tab-${tab.id}`;
-          return (
-            <TabView key={key} window={window} tab={tab} mutator={tabMutator} />
-          );
-        })}
-      </div>
-    );
-  }
-
-  private renderDisclosureButton() {
-    const w = this.props.window;
-    const iconSrc = w.expanded ? Icons.ArrowDown : Icons.ArrowRight;
-    const iconText = w.expanded ? 'Collapse' : 'Expand';
-    const iconStyles = ['tool', 'icon', w.visible ? '' : 'hidden'];
-    return (
-      <img
-        id="disclosure"
-        alt={`window-disclosure-${w.expanded ? 'expanded' : 'collapsed'}`}
-        className={iconStyles.join(' ')}
-        src={iconSrc}
-        title={iconText}
-        onClick={this.handleToggleCollapse}
-      />
-    );
-  }
-
-  private renderInputTitle(window: BT.Window): JSX.Element {
-    const title = window.title;
-
-    return (
-      <InputForm
-        className="window-title"
-        text={title}
-        onSubmit={this.handleSubmitRename}
-        onCancel={this.handleCancelRename}
-      />
-    );
-  }
-
-  private renderStaticTitle(window: BT.Window): JSX.Element {
-    const tabsStr = window.expanded ? '' : ' (' + window.tabs.length + ' tabs)';
-    const title = window.title || 'Window';
-    const fullscreen = window.state === 'fullscreen' ? '(fullscreen)' : '';
-
-    return (
-      <span className="window-title" onClick={this.handleStartRename}>
-        {title} <span>{tabsStr}</span> <span>{fullscreen}</span>
-      </span>
-    );
-  }
-
-  private renderTools() {
-    if (this.state.toolsVisible) {
-      return (
-        <TabToolsView
-          actionIconVisibility={{ rename: true, delete: true, copy: true }}
-          onRenameAction={this.handleStartRename}
-          onDeleteAction={this.handleDelete}
-          onCopyAction={this.handleCopy}
-        />
-      );
-    } else {
-      return null;
-    }
-  }
-
-  ////
-
-  private handleCopy() {
-    this.props.onCopy(this.props.window.id);
-  }
-
-  private handleToggleVisibility() {
-    this.props.window.visible
-      ? this.props.windowMutator.hideWindow(this.props.window.id)
-      : this.props.windowMutator.showWindow(this.props.window.id);
-  }
-
-  private handleDelete() {
-    this.props.windowMutator.deleteWindow(this.props.window.id);
-  }
-
-  private handleStartRename() {
-    this.setState({ renaming: true });
-  }
-
-  private handleCancelRename() {
-    this.setState({ renaming: false });
-  }
-
-  private handleSubmitRename(text: string) {
-    this.props.windowMutator.renameWindow(this.props.window.id, text);
-    this.setState({ renaming: false });
-  }
-
-  private handleToggleCollapse() {
-    if (this.props.window.expanded) {
-      this.props.windowMutator.collapseWindow(this.props.window.id);
-    } else {
-      this.props.windowMutator.expandWindow(this.props.window.id);
-    }
-  }
-
-  private showTools() {
-    this.setState({ toolsVisible: true });
-  }
-  private hideTools() {
-    this.setState({ toolsVisible: false });
-  }
+function renderTools(props: Props, setRenaming: (b: boolean) => void) {
+  return (
+    <TabToolsView
+      actionIconVisibility={{ rename: true, delete: true, copy: true }}
+      onRenameAction={() => setRenaming(true)}
+      onDeleteAction={() => props.windowMutator.deleteWindow(props.window.id)}
+      onCopyAction={() => props.onCopy(props.window.id)}
+    />
+  );
 }
