5240a86096582a20c321d1d02a3691be583390f8
JulianG <juliangaramendy@gmail.com>
2019-02-11 18:36:33 +0000

    two more components refactored into function components with hooks or refs

--- src/view/TextWindowView.tsx
+++ src/view/TextWindowView.tsx
@@ -7,49 +7,42 @@ interface Props {
   onClose(): void;
 }
 
-export class TextWindowView extends React.Component<Props> {
-  private textAreaRef: HTMLTextAreaElement | null | undefined;
+export const TextWindowView = ({ windows, onClose }: Props) => {
+  const textAreaRef = React.useRef<HTMLTextAreaElement>(null);
+  const text = windowsToString(windows);
 
-  constructor(props: Props) {
-    super(props);
-    this.copyToClipboardAndClose = this.copyToClipboardAndClose.bind(this);
-  }
+  React.useEffect(
+    () => {
+      textAreaRef.current && textAreaRef.current.select();
+    },
+    [text]
+  );
 
-  render() {
-    const text = windowsToString(this.props.windows);
-    const rows = text.split('\n').length;
-    return (
-      <div className="textsession" data-testid="text-window-view">
-        <textarea
-          role="input"
-          ref={ref => (this.textAreaRef = ref)}
-          readOnly={true}
-          rows={rows}
-          autoComplete="off"
-          wrap="off"
-          value={text}
-        />
-        <div className="command-buttons">
-          <button className="ok" onClick={this.copyToClipboardAndClose}>
-            Copy to clipboard
-          </button>
-          <span />
-          <button className="cancel" onClick={this.props.onClose}>
-            Go back
-          </button>
-        </div>
-      </div>
-    );
-  }
-
-  componentDidMount() {
-    if (this.textAreaRef) {
-      this.textAreaRef.select();
-    }
-  }
-
-  private copyToClipboardAndClose() {
+  const copyToClipboardAndClose = () => {
     document.execCommand('copy');
-    this.props.onClose();
-  }
-}
+    onClose();
+  };
+
+  return (
+    <div className="textsession" data-testid="text-window-view">
+      <textarea
+        role="input"
+        ref={textAreaRef}
+        readOnly={true}
+        rows={text.split('\n').length}
+        autoComplete="off"
+        wrap="off"
+        value={text}
+      />
+      <div className="command-buttons">
+        <button className="ok" onClick={copyToClipboardAndClose}>
+          Copy to clipboard
+        </button>
+        <span />
+        <button className="cancel" onClick={onClose}>
+          Go back
+        </button>
+      </div>
+    </div>
+  );
+};
