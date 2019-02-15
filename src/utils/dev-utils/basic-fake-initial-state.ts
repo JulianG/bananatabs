f11194d7f55e716d2f9e58c30a7202b7983111c5
JulianG <juliangaramendy@gmail.com>
2019-02-15 18:12:01 +0000

    simpler initial state

---
+++ src/utils/dev-utils/basic-fake-initial-state.ts
@@ -0,0 +1,18 @@
+/*
+ * NOTES:
+ * The visibility of visibile and invisible windows in the stored list
+ * won't be respected. Instead the visibility will depend on
+ * whether or not each stored window is matched to a live window.
+ */
+
+export const stored: string = `Only window:
+* http://eino.info
+* https://zetta.org/
+* https://kathleen.com
+`;
+
+export const live: string = `Only window:
+* http://eino.info
+* https://zetta.org/
+* https://kathleen.com
+`;
